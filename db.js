2d159d13ae29eaffcfa8045315d6fc7270515472
henry <zizimoos@gmail.com>
2019-09-23 17:03:48 +0900

    #3.3

--- db.js
+++ db.js
@@ -1,44 +1,17 @@
-export const videos = [
-  {
-    id: 324393,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://www.youtube.com/embed/9pdj4iJD08s",
-    creator: {
-      id: 121212,
-      name: "Nicolas",
-      email: "nico@las.com"
-    }
-  },
-  {
-    id: 1212121,
-    title: "Video super",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://www.youtube.com/embed/9pdj4iJD08s",
-    id: 121212,
-    name: "Nicolas",
-    email: "nico@las.com"
-  },
-  {
-    id: 55555,
-    title: "Video nice",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://www.youtube.com/embed/9pdj4iJD08s",
-    id: 121212,
-    name: "Nicolas",
-    email: "nico@las.com"
-  },
-  {
-    id: 11111,
-    title: "Video perfect",
-    description: "This is something I love",
-    views: 24,
-    videoFile: "https://www.youtube.com/embed/9pdj4iJD08s",
-    id: 121212,
-    name: "Nicolas",
-    email: "nico@las.com"
-  }
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Video";
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
--- init.js
+++ init.js
@@ -1,7 +1,9 @@
+import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 4000;
-const handleListening = () =>
-  console.log(`✅  Listening on : http://localhost:${PORT}`);
+const PORT = process.env.PORT;
+const handleListening = () => console.log(`✅  Listening on : http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
