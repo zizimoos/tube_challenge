78586a640a03f0cad7507b5dce4e27add29eeba3
zizimoos_iMac <zizimoos@gmail.com>
2019-09-30 01:17:13 +0900

    #5.2

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,8 +40,11 @@ export const logout = (req, res) => {
 
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 
-export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "UserDetail" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "UserDetail" });
 
-export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "EditProfile" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "EditProfile" });
+
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "ChangePassword" });
